4360ef6cdcb48f7b1487f937e6e8cfe5ff66a5b9
mohan <mohankumarmukunda@gmail.com>
2023-03-27 13:10:06 +0200

    dash with youtube link

--- notebooks/app.py
+++ notebooks/app.py
@@ -4,6 +4,8 @@ from dash import dcc
 from dash.dependencies import Input, Output
 import pandas as pd
 import plotly.express as px
+import plots_functions as pf
+import neuralxpresso as nx
 
 
 app = dash.Dash(__name__)
@@ -12,18 +14,25 @@ app.layout = html.Div([
     dcc.Input(id='input', type='text', value=''),
     dcc.Graph(id='graph1'),
     dcc.Graph(id='graph2'),
+    dcc.Graph(id='graph3')
     
 ])
 
+
+
 @app.callback(
-    [Output('graph1', 'figure'),Output('graph2', 'figure')],
+    [Output('graph1', 'figure'),Output('graph2', 'figure'), Output('graph3','figure')],
     [Input('input', 'value')]
 )
+
+
 def update_graph(value):
     # Convert the user input to a numpy array
-    df_plotting = pd.read_csv("/Users/mohan/neuefische/NeuralXpresso/data/plotting.csv")
+    #df_plotting = pd.read_csv("/Users/mohan/neuefische/NeuralXpresso/data/plotting.csv")
+    nxp = nx.NeuralXpressoSession(yt_link = value)
+    df_plotting = nxp.run_analysis()
 
-    fig1 = px.bar(df_plotting, x="pos_sec", y='probability', color = 'emotion',
+    fig1 = px.bar(df_plotting, x="frame", y='probability', color = 'emotion',
              barmode="stack", title="Distribution of Emotions across Frames (every 10th frame)")
     fig1.update_layout(xaxis_title="Frame", yaxis_title="Emotion Distribution",     title={
         'y': 0.9,
@@ -34,10 +43,13 @@ def update_graph(value):
     
     df = df_plotting
     df['moving_avg'] = df['probability'].rolling(window=10, center=True).mean()
-    fig2 = px.area(df, x="pos_sec", y="moving_avg", color="emotion")
+    fig2 = px.area(df, x="frame", y="moving_avg", color="emotion")
+    #fig2 = pf.ThreeDimensionalGraph(df_plotting)
+
+    fig3 = pf.hist_max_prob_emo(df_plotting)
     
 
-    return [fig1,fig2]
+    return [fig1,fig2,fig3]
 
 
 if __name__ == '__main__':
