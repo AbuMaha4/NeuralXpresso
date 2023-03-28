bf0b27e23b8b19530c7513be0e11f690f63c0fb7
Maha Abu-Khousa <120604499+AbuMaha4@users.noreply.github.com>
2023-03-28 14:35:18 +0200

    pyfile with functions and updated app.py version 2

--- notebooks/plots.py
+++ notebooks/plots.py
@@ -6,24 +6,143 @@ import cv2
 
 
 def get_pallette():
-    return ['#CA3435', '#ff7f00', '#9370DB', '#40E0D0', '#1f77b4', '#9467bd', '#7f7f7f']
+    return ['#CA3435', '#00ff00', '#0000ff', '#ff0000', '#ffff00', '#800080', '#ffa500']
 
 
 
-def get_overview_normalized(df_video):
-    df_video['normalized_probability'] = df_video.groupby('frame')['probability'].apply(lambda x: x / x.sum())
+def overview_plot(df_video):
+    cb_palette = get_pallette()
+    grouped_df = df_video.groupby(['frame', 'emotion'])['probability'].sum().unstack()
+    normalized_df = grouped_df.div(grouped_df.sum(axis=1), axis=0)
+    normalized_df = normalized_df.rolling(window=10).mean().dropna()
+    normalized_df.index = normalized_df.index.astype(str)
+    fig = px.area(normalized_df, x=normalized_df.index, y=normalized_df.columns,
+                color_discrete_sequence=cb_palette)
 
-    fig = px.bar(df_video, x="frame", y='normalized_probability', color='emotion',
-              barmode="stack", title="Distribution of Emotions across Frames")
-    fig.update_layout(xaxis_title="Frame", yaxis_title="Emotion Distribution",
-                   title={
-                       'y': 0.9,
-                       'x': 0.5,
-                       'xanchor': 'center',
-                       'yanchor': 'top'
-                   })
+    fig.update_layout(
+            title={
+                'text': 'Probability of emotion per frame',
+                'font': {'size': 24, 'color': 'black'}
+                #'x': 0.5, #center header
+                #'y': 0.95 #center header
+            },
+            xaxis_title='Frames',
+            yaxis_title='Probability',
+            legend_title='Emotion',
+            font=dict(family='Arial', size=14),
+            margin=dict(l=50, r=50, t=100, b=50),
+            plot_bgcolor='white',
+            xaxis=dict(dtick=1),  #set the x-axis step 
+            yaxis=dict(
+            title='Probability',
+            title_standoff=3
+        )
+        )
+        
+
+    # Update the legend with customizations
+    fig.update_traces(
+        hovertemplate='<br>'.join([
+            'Emotion: %{fullData.name}',
+            'Frame: %{x:.2f} ',
+            'Probability: %{y:.2f}'
+        ]),
+        hoverlabel=dict(bgcolor='white', font_size=14),
+        line=dict(width=2),
+        showlegend=True,
+        stackgroup='one',
+        hoverinfo='all',
+
+    )
 
     return fig
+
+def get_aggregated_emotion_counts(df_video, df_character):
+    # Get unique person IDs
+    unique_person_ids = df_video['person_ID'].unique()
+
+    
+    # Initialize an empty DataFrame with all emotions as index
+    aggregated_counts = pd.DataFrame(columns=['index', 'emotion'])
+    aggregated_counts['index'] = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Sad', 'Surprise']
+    aggregated_counts['emotion'] = 0
+
+    for ID in df_character.person_ID:
+        if (df_character.loc[df_character['person_ID'] == ID].appearances.values[0] > (0.2 * df_character.appearances.sum())):
+            # Iterate over all unique person IDs and sum their emotion counts
+            df_single_person = get_df_single_person(df_video, ID=ID)
+            df_radar = get_df_radar(df_single_person)
+            aggregated_counts['emotion'] += df_radar['emotion']
+
+    return aggregated_counts
+
+
+
+def get_overall_overview(df_video, df_character):
+
+    df_aggregated_emotion_counts = get_aggregated_emotion_counts(df_video, df_character)
+    
+    
+    
+    fig_radar = get_radar_plot_overview(df_aggregated_emotion_counts)
+    fig_normalized = overview_plot(df_video)
+    fig = make_subplots(rows=1, cols=2, column_widths=[0.7, 0.3], specs=[[{'type': 'xy'}, {'type': 'polar'}]], horizontal_spacing=0.1, vertical_spacing=0.2)
+
+    # Add each trace object in fig_normalized.data to the subplot one by one
+    for trace in fig_normalized.data:
+        fig.add_trace(trace, row=1, col=1)
+
+    fig.add_trace(fig_radar.data[0], row=1, col=2)
+
+
+    fig.update_layout(
+            polar=dict(
+                radialaxis=dict(
+                    showticklabels=False,
+                    
+                ),
+                angularaxis=dict(
+                    tickfont=dict(size=14),
+                    rotation=90,
+                    direction='clockwise'
+                )
+            ),
+            showlegend=True,
+            legend=dict(
+            x=1.1,
+            y=0.5,
+            xanchor='left',
+            yanchor='middle',
+            title_font=dict(size=16)
+            ),
+            margin=dict(t=80, b=50, l=50, r=50),
+            font=dict(size=16)
+        )
+
+
+    # Update axis titles
+    fig.update_xaxes(title_text="Frame", row=1, col=1)
+    fig.update_yaxes(title_text="Probability", row=1, col=1)
+    fig.update_xaxes(title_text="Emotion Counts", row=1, col=2)
+    fig.update_yaxes(title_text="Prevalence", row=1, col=2)
+
+    # Add subtitles for each subplot
+    annotations = [
+        dict(
+            x=0.13,
+            y=1.17,
+            xref='paper',
+            yref='paper',
+            text='Emotion-Landscape over Frames',
+            showarrow=False,
+            font=dict(size=18)
+        ),
+    ]
+    fig.update_layout(annotations=annotations)
+
+    # Show the figure
+    return fig
+
 
 
 def get_df_single_person(df_video, ID):
@@ -46,6 +165,7 @@ def get_df_radar(df_single_person):
     complete_df.columns = ['index', 'emotion']
 
     return complete_df
+
 
 
 
@@ -90,6 +210,54 @@ def get_emotion_landscape(df_single_person):
         stackgroup='one',
         hoverinfo='all',
 
+    )
+
+    return fig
+
+
+def get_radar_plot_overview(df_aggregated_emotion_counts):
+    cb_palette = get_pallette()
+
+
+    fig = go.Figure(data=go.Scatterpolar(
+        r=df_aggregated_emotion_counts['emotion'],
+        theta=df_aggregated_emotion_counts['index'],
+        fill='toself',
+        name='Emotion Counts',
+        line_color=cb_palette[0],
+        hovertemplate='%{theta}: %{r}',
+        showlegend=False
+    ))
+    fig.update_xaxes(title_text="Person")
+
+    fig.update_layout(
+        polar=dict(
+            radialaxis=dict(
+                visible=True,
+                range=[0, max(df_aggregated_emotion_counts['emotion'])],
+                showticklabels=False,
+                showgrid=True,
+                ticks=''
+            ),
+            angularaxis=dict(
+                tickfont=dict(size=14),
+                rotation=90,
+                direction='clockwise'
+            )
+        ),
+        showlegend=False,
+        height=300,
+        margin=dict(t=80, b=50, l=50, r=50),
+        font=dict(size=16),
+        title={
+            'text': "Overall Prevalence Of Feelings During The Video",
+            'y':0.95,
+            'x':0.5,
+            'xanchor': 'center',
+            'yanchor': 'top'
+        },
+        paper_bgcolor='white',
+        plot_bgcolor='white'
     )
 
     return fig
